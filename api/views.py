f13ecdd8bd1525c39bdcc0f261b712206d778a6f
Siddharth-Nama <siddharthnama2604@gmail.com>
2025-12-13 02:06:36 +0530

    fix: Restore Auth Views and add Search action

--- api/views.py
+++ api/views.py
@@ -5,7 +5,16 @@ from rest_framework.authtoken.models import Token
 from .serializers import RegisterSerializer, LoginSerializer, SweetSerializer
 from .models import Sweet
 
-# ... (views)
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(views.APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user_id': user.pk, 'email': user.email}, status=status.HTTP_200_OK)
 
 class SweetViewSet(viewsets.ModelViewSet):
     queryset = Sweet.objects.all()
@@ -36,22 +45,3 @@ class SweetViewSet(viewsets.ModelViewSet):
 
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
-    serializer_class = RegisterSerializer
-
-class LoginView(views.APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key, 'user_id': user.pk, 'email': user.email}, status=status.HTTP_200_OK)
-
-class SweetViewSet(viewsets.ModelViewSet):
-    queryset = Sweet.objects.all()
-    serializer_class = SweetSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_permissions(self):
-        if self.action == 'destroy':
-            return [permissions.IsAdminUser()]
-        return super().get_permissions()
